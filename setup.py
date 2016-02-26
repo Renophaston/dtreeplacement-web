eaa0faf728cb2ab159317795c9c02872d48773ae
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-02-26 15:48:18 -0800

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dtreeplacement-web',
+    version='0.1',
+    packages=['dtreeplacement-web'],
+    url='https://github.com/Renophaston/dtreeplacement-web',
+    license='The MIT License (MIT)',
+    author='Renophaston',
+    author_email='',
+    description='Third attempt at a replacement for the way I use Treeline, web-interface'
+)

b11b9a5eb24642c682185de2b1a041713bee3140
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-03-06 11:59:28 -0800

    This is such a messy commit
    I've been experimenting and learning, and it's a horrible mess but I did some stuff. Sorry.

--- dtreeplacement_web/models.py
+++ dtreeplacement_web/models.py
@@ -10,7 +10,7 @@ class Item(db.Model):
     # id, primary key
     id = db.Column(db.Integer, primary_key=True)
     # the content of the item, like a book title, currently just a string
-    content = db.Column(db.Text, unique=True)
+    content = db.Column(db.Text, unique=True, nullable=False)
     created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_timestamp = db.Column(
         db.DateTime,
@@ -24,4 +24,24 @@ class Item(db.Model):
     #     super(Item, self).__init__(*args, **kwargs)
 
     def __repr__(self):
-        return '<Item: %s>' % self.content
+        return '<Item: {}>'.format(self.content)
+
+
+class Membership(db.Model):
+    """Connects an Item to another Item in a group-member relation."""
+    # id, primary key
+    id = db.Column(db.Integer, primary_key=True)
+    # id of Item acting as group/parent
+    group_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    # id of Item acting as member/child
+    member_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    # only include a relationship in this table once
+    __table_args__ = (
+        db.UniqueConstraint(group_id, member_id),
+    )
+
+    def __repr__(self):
+        return '<Membership: {0} is in group {1}>'.format(
+            self.member_id,
+            self.group_id
+        )
--- dtreeplacement_web/views.py
+++ dtreeplacement_web/views.py
@@ -1,14 +1,50 @@
 """Views and routes for the UI."""
 
-from app import app
+from app import app, db
 from flask import render_template
+from models import Item, Membership
+from sqlalchemy.orm.exc import NoResultFound
 
 
 @app.route('/')
 def index():
-    return render_template('index.j2', title='Index', message='Welcome.')
+    items = Item.query.all()
+    return render_template(
+        'index.j2',
+        title='Index',
+        message='Welcome.',
+        items=items
+    )
 
 
-@app.route('/test')
-def intext():
-    return render_template('index.j2', title='Intext', message='Testing.')
+@app.route('/createtables')
+def create_tables():
+    db.create_all()
+    return render_template(
+        'index.j2',
+        title='Index',
+        message='ATTEMPTED to create tables, anyway.'
+    )
+
+
+@app.route('/item')
+def item_index():
+    items = Item.query.all()
+    return render_template('item.j2', items=items)
+
+
+@app.route('/item/<int:item_id>')
+def item_view(item_id):
+    try:
+        item = Item.query.filter(Item.id == item_id).one()
+    except NoResultFound as ex:
+        item = None
+    if item is None:
+        message = 'No fucking items matching id {}.'.format(item_id)
+    else:
+        message = 'Item {}: {}'.format(item_id, item.content)
+    return render_template(
+        'index.j2',
+        title='Item',
+        message=message
+    )

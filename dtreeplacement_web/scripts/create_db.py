11e1f1f2c01c46b44a9e9f82b83cb51d3a4edacc
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-03-07 14:00:07 -0800

    Removed the scripts dir and create_db script
    I'm not going to do much via the Python console; I'll just do it in the app itself. I might end up doing something similar to this, but not now.

--- dtreeplacement_web/scripts/create_db.py
+++
@@ -1,9 +0,0 @@
-"""Generate database from code in models.py."""
-
-import os, sys
-sys.path.append(os.getcwd())  # before import db, so we can see app
-from app import db
-
-
-if __name__ == '__main__':
-    db.create_all()

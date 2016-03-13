72a0ec588a10d6df9fa16389b396616d30bdabee
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-03-13 11:26:27 -0700

    Very bare item index and item_detail views working

--- dtreeplacement_web/items/blueprint.py
+++ dtreeplacement_web/items/blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from models import Item
 
 items = Blueprint('items', __name__, template_folder='templates')
@@ -6,9 +6,11 @@ items = Blueprint('items', __name__, template_folder='templates')
 
 @items.route('/')
 def index():
-    return 'Items index'
+    all_items = Item.query.all()
+    return render_template('items/index.j2', items=all_items)
 
 
 @items.route('/<int:item_id>')
 def item_detail(item_id):
-    return 'Item detail view for item {}.'.format(item_id)
+    item = Item.query.filter(Item.id == item_id).first()
+    return render_template('items/item_detail.j2', item=item)

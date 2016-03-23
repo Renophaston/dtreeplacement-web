c1f10aa5bc423b6d4ca53011909bed8a0b16269e
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-03-23 15:53:52 -0700

    Sort item list

--- dtreeplacement_web/items/blueprint.py
+++ dtreeplacement_web/items/blueprint.py
@@ -6,7 +6,7 @@ items = Blueprint('items', __name__, template_folder='templates')
 
 @items.route('/')
 def index():
-    all_items = Item.query.all()
+    all_items = Item.query.order_by(Item.content.asc()).all()
     return render_template('items/index.j2', items=all_items)
 
 
